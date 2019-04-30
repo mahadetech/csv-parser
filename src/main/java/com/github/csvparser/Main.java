c84dea3930eafa90ed29993e71aa2d32bd6680f1
mahalakshmi82@gmail.com <mahalakshmi82@gmail.com>
2019-04-30 21:36:41 +0200

    initia commit

--- src/main/java/com/github/csvparser/Main.java
+++ src/main/java/com/github/csvparser/Main.java
@@ -2,4 +2,12 @@ package com.github.csvparser;
 
 // MY ist project
 public class Main {
+
+    // 1. Read the CSV file
+
+    // 2. Read each line and convert it to the java object (User)
+
+    // 3. Print the Java object
+
+    // 4. Find the User who has the maximum age and height and print this object
 }
---
+++ src/main/java/com/github/csvparser/models/User.java
@@ -0,0 +1,67 @@
+package com.github.csvparser.models;
+
+public final class User {
+    private int id;
+    private String name;
+    private String color;
+    private String country;
+    private int age;
+    private double height;
+    private double weight;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
