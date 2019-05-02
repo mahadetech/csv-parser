73522d8f098d4a234793458273bdfcd8540c1f1d
mahadetech <mahalakshmi82@gmail.com>
2019-05-02 20:55:05 +0200

    commited

--- src/main/java/com/github/csvparser/Main.java
+++ src/main/java/com/github/csvparser/Main.java
@@ -1,30 +1,47 @@
 package com.github.csvparser;
-import java.io.IOException;
-import java.io.Writer;
+import java.io.BufferedReader;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
-import com.github.csvparser.models.BasicCsvReader;
+import com.github.csvparser.models.User;
 import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVPrinter;
-
-
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 
 
 public class Main {
 
-    public static void main(String[] args) throws Exception
-    {
+    public static void main(String[] args) throws Exception {
+        Main a = new Main();
+        a.readCSV();
+    }
 
+    public void readCSV() throws Exception {
+        Path fiiePath = Paths.get(ClassLoader.getSystemResource("users.csv").toURI());
+        BufferedReader reader = Files.newBufferedReader(fiiePath);
+        CSVParser csvParser = CSVFormat.EXCEL.withFirstRecordAsHeader().parse(reader);
 
-            BasicCsvReader a =new BasicCsvReader();
-            //a.readcsv();
-            a.csvread();
+        AtomicReference<List<User>> userList = new AtomicReference<>(new ArrayList<User>());
 
+        for (CSVRecord csvRecord : csvParser) {
+            // Accessing Values by Column Index
+            String name = csvRecord.get("name");
+            int id = Integer.parseInt(csvRecord.get("id"));
+            int age = Integer.parseInt(csvRecord.get("age"));
+            int height = Integer.parseInt(csvRecord.get("height"));
+            int weight = Integer.parseInt(csvRecord.get("weight"));
+            String color = csvRecord.get("color");
+            String country = csvRecord.get("country");
 
+            User user = new User(id, name, age, height, weight, color, country);
+            userList.get().add(user);
 
-
+            System.out.println(user);
+        }
     }
 }
 
